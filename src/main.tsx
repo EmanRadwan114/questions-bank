81e8ac3d1a23b243070b91abbc6861702385126b
Eman Radwan <emanalaa11499@gmail.com>
2025-11-30 13:44:50 +0200

    initialize vite project

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "@fontsource/oswald/400.css";
 import App from "./App.tsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <ToastContainer />
   </StrictMode>
 );
